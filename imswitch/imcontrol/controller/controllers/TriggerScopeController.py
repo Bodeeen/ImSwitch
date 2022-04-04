e457da7905c4754d9d936ecf9c1e66da9e29afbf
Andreas Bodén <andreas.boden@gmail.com>
2022-04-04 12:11:35 +0300

    Additional changes in Triggerscope widget!

--- imswitch/imcontrol/controller/controllers/TriggerScopeController.py
+++ imswitch/imcontrol/controller/controllers/TriggerScopeController.py
@@ -10,24 +10,25 @@ class TriggerScopeController(ImConWidgetController):
         super().__init__(*args, **kwargs)
         # Connect PositionerWidget signals
         self._widget.sigRunToggled.connect(self.run)
+        self._widget.voltageEdit.valueChanged.connect(self.changeVoltag)
+
+    def changeVoltag(self, newV):
+        self._master.triggerScopeManager.sendAnalog(1, newV)
 
     def run(self):
-        #self._master.triggerScopeManager.send("*", 1)
-        self.increment = float(self._widget.incrementVoltage.text())
-        self.slope = float(self._widget.setSlope.text())
-        self.finalVoltage = float(self._widget.setVoltage.value())
-        self.current = float(self._widget.currentVoltage.text())
-        self._widget.currentVoltage.setText(str(round(self.finalVoltage, 3)))
-        steps = int(np.ceil(np.abs(self.finalVoltage - self.current) / self.slope)) + 1
-        self.TTLdelay = int(self._widget.setTTLtime.text())
-        self.DACdelay = int(self._widget.setDACtime.text())
-        self.Repetition = int(self._widget.setREP.text())
-        dacarray = np.linspace(self.current, self.finalVoltage, steps)
+        currentV = self._widget.voltageEdit.value()
+        stepsSize = self._widget.stepSizeEdit.value()
+        steps = self._widget.nrOfStepsEdit.value()
+        finalV = currentV + steps * stepsSize
+        dacarray = np.linspace(currentV, finalV, steps)
         ttlarray = np.ones(steps, dtype=int)
-        params = self.setParams(1, 1, len(dacarray), 0, self.DACdelay, self.TTLdelay, self.Repetition)
+
+        stepTime = self._widget.stepTimeEdit.value()
+        TTLTile = self._widget.TTLTimeEdit.value()
+        repetitions = self._widget.repEdit
+        params = self.setParams(1, 1, len(dacarray), 0, stepTime, TTLTile, repetitions)
         self._master.triggerScopeManager.run_wave(dacarray, ttlarray, params)
 
-        #self._master.triggerScopeManager.sendAnalog(1, 1)
 
     def setParams(self, analogLine, digitalLine, length, trigMode, delayDAC, delayTTL, reps):
         params = dict([])
--- imswitch/imcontrol/view/widgets/TriggerScopeWidget.py
+++ imswitch/imcontrol/view/widgets/TriggerScopeWidget.py
@@ -1,72 +1,78 @@
 from qtpy import QtCore, QtWidgets
 
-from imswitch.imcontrol.view import guitools
-from .basewidgets import Widget
+# from imswitch.imcontrol.view import guitools
+# from .basewidgets import Widget
 
 
-class TriggerScopeWidget(Widget):
+class TriggerScopeWidget(QtWidgets.QWidget):
     """ Widget for controlling the parameters of a TriggerScope. """
     sigRunToggled = QtCore.Signal(float)  # (enabled)
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # self.runButton = guitools.BetterPushButton('Run')
-        # self.runButton.setCheckable(True)
-        # self.runButton.clicked.connect(self.sigRunToggled)
-        # grid.addWidget(self.runButton, 0, 0)
+        self.quickSetVoltageLabel = QtWidgets.QLabel('Set voltage')
+        self.quickSetVoltageLabel.setStyleSheet("font-weight: bold")
 
+        self.voltageLabel = QtWidgets.QLabel("Voltage")
+        self.voltageEdit = QtWidgets.QDoubleSpinBox()
+        self.voltageEdit.setMinimum(-5)
+        self.voltageEdit.setMaximum(5)
+        self.voltageEdit.setSingleStep(0.1)
+
+        self.sequenceLabel = QtWidgets.QLabel('Run scan sequence')
+        self.sequenceLabel.setStyleSheet("font-weight: bold")
+
+        self.stepSizeLabel = QtWidgets.QLabel("Step size [V]")
+        self.stepSizeEdit = QtWidgets.QDoubleSpinBox()
+        self.stepSizeEdit.setMinimum(-5)
+        self.stepSizeEdit.setSingleStep(0.1)
+
+        self.nrOfStepsLabel = QtWidgets.QLabel("Steps in scan")
+        self.nrOfStepsEdit = QtWidgets.QSpinBox()
+
+        self.TTLtimeLabel = QtWidgets.QLabel("TTL time [ms]")
+        self.TTLtimeEdit = QtWidgets.QDoubleSpinBox()
+        self.TTLtimeEdit.setSingleStep(0.1)
+
+
+        self.stepTimeLabel = QtWidgets.QLabel("Step time [ms]")
+        self.stepTimeEdit = QtWidgets.QDoubleSpinBox()
+        self.stepTimeEdit.setSingleStep(0.1)
+
+        self.repLabel = QtWidgets.QLabel("Repetitions")
+        self.repEdit = QtWidgets.QSpinBox()
+
+        self.runButton = QtWidgets.QPushButton('Run')
+        self.runButton.clicked.connect(self.sigRunToggled.emit)
+
+        #Set widget layout
         grid = QtWidgets.QGridLayout()
         self.setLayout(grid)
-        self.incrementVoltageLabel = QtWidgets.QLabel("Increment/V")
-        grid.addWidget(self.incrementVoltageLabel, 0, 0)
 
-        self.incrementVoltage = QtWidgets.QLineEdit()
-        grid.addWidget(self.incrementVoltage, 0, 1)
-        self.incrementVoltage.textChanged[str].connect(self.action)
+        grid.addWidget(self.quickSetVoltageLabel, 0, 0)
+        grid.addWidget(self.voltageLabel, 1, 0)
+        grid.addWidget(self.voltageEdit, 1, 1)
+        grid.addWidget(self.sequenceLabel, 2, 0)
+        grid.addWidget(self.stepSizeLabel, 3, 0)
+        grid.addWidget(self.stepSizeEdit, 3, 1)
+        grid.addWidget(self.nrOfStepsLabel, 3, 2)
+        grid.addWidget(self.nrOfStepsEdit, 3, 3)
+        grid.addWidget(self.TTLtimeLabel, 4, 0)
+        grid.addWidget(self.TTLtimeEdit, 4, 1)
+        grid.addWidget(self.stepTimeLabel, 4, 2)
+        grid.addWidget(self.stepTimeEdit, 4, 3)
+        grid.addWidget(self.repLabel, 5, 0)
+        grid.addWidget(self.repEdit, 5, 1)
+        grid.addWidget(self.runButton, 5, 2)
 
-        self.setVoltageLabel = QtWidgets.QLabel("Set Voltage/V")
-        grid.addWidget(self.setVoltageLabel, 0, 2)
-
-        self.setVoltage = QtWidgets.QDoubleSpinBox()
-        self.setVoltage.setKeyboardTracking(False)
-        self.setVoltage.setDecimals(3)
-        self.setVoltage.setMinimum(-5.0)
-        self.setVoltage.setMaximum(5.0)
-        self.setVoltage.valueChanged.connect(self.sigRunToggled)
-        grid.addWidget(self.setVoltage, 0, 3)
-
-        self.TTLtimeLabel = QtWidgets.QLabel("TTL Time/ms")
-        grid.addWidget(self.TTLtimeLabel, 0, 4)
-
-        self.setTTLtime = QtWidgets.QLineEdit()
-        grid.addWidget(self.setTTLtime, 0, 5)
-
-        self.setSlopeLabel = QtWidgets.QLabel("Slope/V")
-        grid.addWidget(self.setSlopeLabel, 1, 0)
-
-        self.setSlope = QtWidgets.QLineEdit()
-        grid.addWidget(self.setSlope, 1, 1)
-
-        self.currentVoltageLabel = QtWidgets.QLabel("Current Voltage/V")
-        grid.addWidget(self.currentVoltageLabel, 1, 2)
-
-        self.currentVoltage = QtWidgets.QLineEdit()
-        self.currentVoltage.setReadOnly(True)
-        self.currentVoltage.setText(str(0))
-        grid.addWidget(self.currentVoltage, 1, 3)
-
-        self.DACtimeLabel = QtWidgets.QLabel("DAC Time/ms")
-        grid.addWidget(self.DACtimeLabel, 1, 4)
-
-        self.setDACtime = QtWidgets.QLineEdit()
-        grid.addWidget(self.setDACtime, 1, 5)
-
-        self.RepLabel = QtWidgets.QLabel("Repetition")
-        grid.addWidget(self.RepLabel, 1, 6)
-
-        self.setREP = QtWidgets.QLineEdit()
-        grid.addWidget(self.setREP, 1, 7)
     def action(self):
         self.setVoltage.setSingleStep(float(self.incrementVoltage.text()))
 
+
+if __name__ == '__main__':
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    wid = TriggerScopeWidget()
+    wid.show()
+    sys.exit(app.exec_())
