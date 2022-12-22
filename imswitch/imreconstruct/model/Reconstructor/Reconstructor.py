3199deb8d6aaa657209422924bb4b6d31d2640ad
Snouty <Snouty@TestaLab3.ad.scilifelab.se>
2022-12-22 18:52:40 +0100

    Added new reconstruction code in Reconstructor

--- imswitch/imreconstruct/model/Reconstructor/Reconstructor.py
+++ imswitch/imreconstruct/model/Reconstructor/Reconstructor.py
@@ -4,60 +4,11 @@ import cupy as cp
 from cupyx.scipy import signal as cpsig
 from cupyx.scipy.ndimage import affine_transform
 from numba import cuda
-
+from .gpu_kernels import gaussDistribTransform
 from imswitch.imcommon.model import dirtools, initLogger
 
 mempool = cp.get_default_memory_pool()
 pinned_mempool = cp.get_default_pinned_memory_pool()
-
-""" Forward model """
-
-
-@cuda.jit
-def NNTransform(dataStack, sampleVol, transformMat, offset):
-    """Retrieve the nearest neighbour value from the samplevolume and place in the datastack"""
-    idz, idy, idx = cuda.grid(3)
-    if idz < dataStack.shape[0] and idy < dataStack.shape[1] and idx < dataStack.shape[2]:
-        sampleCoords_z = transformMat[0, 0] * idz + transformMat[0, 1] * idy + transformMat[0, 2] * idx
-        sampleCoords_y = transformMat[1, 0] * idz + transformMat[1, 1] * idy + transformMat[1, 2] * idx
-        sampleCoords_x = transformMat[2, 0] * idz + transformMat[2, 1] * idy + transformMat[2, 2] * idx
-
-        # Round to nearest and cast to int
-        sampleIndex_z = int(round(sampleCoords_z))
-        sampleIndex_y = int(round(sampleCoords_y))
-        sampleIndex_x = int(round(sampleCoords_x))
-
-        if 0 <= sampleIndex_z < sampleVol.shape[0] and 0 <= sampleIndex_y < sampleVol.shape[
-            1] and 0 <= sampleIndex_x < \
-                sampleVol.shape[2]:
-            value = sampleVol[sampleIndex_z, sampleIndex_y, sampleIndex_x] + offset
-            dataStack[idz, idy, idx] = value
-        cuda.syncthreads()
-
-
-"""Inverse model"""
-
-
-@cuda.jit
-def invNNTransform(dataStack, sampleVol, transformMat, offset):
-    """Distribute the values in the data stack back to the sample canvas in the nearest neighbour voxel"""
-    idz, idy, idx = cuda.grid(3)
-    if idz < dataStack.shape[0] and idy < dataStack.shape[1] and idx < dataStack.shape[2]:
-        sampleCoords_z = transformMat[0, 0] * idz + transformMat[0, 1] * idy + transformMat[0, 2] * idx
-        sampleCoords_y = transformMat[1, 0] * idz + transformMat[1, 1] * idy + transformMat[1, 2] * idx
-        sampleCoords_x = transformMat[2, 0] * idz + transformMat[2, 1] * idy + transformMat[2, 2] * idx
-
-        # Round to nearest and cast to int
-        sampleIndex_z = int(round(sampleCoords_z))
-        sampleIndex_y = int(round(sampleCoords_y))
-        sampleIndex_x = int(round(sampleCoords_x))
-
-        if 0 <= sampleIndex_z < sampleVol.shape[0] and 0 <= sampleIndex_y < sampleVol.shape[
-            1] and 0 <= sampleIndex_x < \
-                sampleVol.shape[2]:
-            value = dataStack[idz, idy, idx] - offset
-            cuda.atomic.add(sampleVol, (sampleIndex_z, sampleIndex_y, sampleIndex_x), value)
-        cuda.syncthreads()
 
 class Reconstructor:
     """ This class takes the raw data together with pre-set
@@ -69,83 +20,75 @@ class Reconstructor:
         self.__logger = initLogger(self)
 
     def simpleDeskew(self, data, cam_px_size, alpha_rad, dy_step_size, recon_vx_size):
-        """Extracts the signal of the data according to given parameters.
-        Output is a 4D matrix where first dimension is base and last three
-        are frame and pixel coordinates."""
+        """Deskew the data in one step transform"""
+
+        camera_offset = 100
+
+        """Make coordiate transformation matrix such that sampleCoordinates = M * dataCoordinates"""
+        transformation_mat = cp.array([[cam_px_size * np.sin(alpha_rad), 0, 0],
+                                       [cam_px_size * np.cos(alpha_rad), dy_step_size, 0],
+                                       [0, 0, cam_px_size]])
+        voxelize_scale_mat = cp.array(
+            [[1 / recon_vx_size, 0, 0], [0, 1 / recon_vx_size, 0], [0, 0, 1 / recon_vx_size]])
+
+        M = cp.matmul(voxelize_scale_mat, transformation_mat)
+
+        """Reshape data"""
+        permuted_axis = (1, 0, 2)
+        data_correct_axes = np.transpose(data, axes=permuted_axis)
+        size_data = data_correct_axes.shape
+        """Calculate reconstruction canvas size"""
+        cp_size_data = cp.array(size_data)
+        cp_size_sample = cp.ceil(cp.matmul(M, cp_size_data)).astype(int)
+        size_sample = cp.asnumpy(cp_size_sample)
+
+        interp_size_fac = 1 # 1 means gauss FWHM = distance to nearest datapoint
+        x_dist_px = cam_px_size / recon_vx_size  # px size in vx
+        y_dist_px = dy_step_size / recon_vx_size
+        z_dist_px = dy_step_size * np.tan(alpha_rad) / recon_vx_size  # distance between planes in vx
+        z_halfsize = int(np.ceil(0.5 * z_dist_px))
+        y_halfsize = int(np.ceil(0.5 * y_dist_px))
+        x_halfsize = int(np.ceil(0.5 * x_dist_px))
+        sigma_z = z_dist_px / 2.355
+        sigma_y = y_dist_px / 2.355
+        sigma_x = x_dist_px / 2.355
+        sigma_z, sigma_y, sigma_x = interp_size_fac * sigma_z, interp_size_fac * sigma_y, interp_size_fac * sigma_x
+
+        free_mem = cuda.current_context().get_memory_info()[0]
+        needed_sample_volumes = 2
+        needed_data_volumes = 1
+        tot_data_elements = needed_sample_volumes*np.prod(size_sample) + needed_data_volumes*np.prod(size_data)
+        f32memusage = 4*tot_data_elements
+        if f32memusage > free_mem:
+            dt = 'float16'
+            print('Low on memory, using 16-bit precision float')
+        else:
+            dt = 'float32'
+        """Reconstruct"""
+        threadsperblock = 8
+        blocks_per_grid_z = (size_data[0] + (threadsperblock - 1)) // threadsperblock
+        blocks_per_grid_y = (size_data[1] + (threadsperblock - 1)) // threadsperblock
+        blocks_per_grid_x = (size_data[2] + (threadsperblock - 1)) // threadsperblock
         try:
-            camera_offset = 100
-
-            permuted_axis = (1, 0, 2)
-            data_correct_axes = np.transpose(data, axes=permuted_axis).astype(float)
-            adjustedData = cp.array(data_correct_axes - camera_offset).clip(0)
-
-            """Make coordiate transformation matrix such that sampleCoordinates = M * dataCoordinates"""
-            transformation_mat = cp.array([[cam_px_size * np.sin(alpha_rad), 0, 0],
-                                           [cam_px_size * np.cos(alpha_rad), dy_step_size, 0],
-                                           [0, 0, cam_px_size]])
-            voxelize_scale_mat = cp.array(
-                [[1 / recon_vx_size, 0, 0], [0, 1 / recon_vx_size, 0], [0, 0, 1 / recon_vx_size]])
-
-            M = cp.matmul(voxelize_scale_mat, transformation_mat)
-
-            """Make reconstruction canvas"""
-            size_data = cp.array(adjustedData.shape)
-            size_data_host = cp.asnumpy(size_data)
-            size_sample = cp.ceil(cp.matmul(M, size_data)).astype(int)
-            invTransfOnes = cp.zeros(cp.asnumpy(size_sample))
-            recon_canvas = cp.zeros(cp.asnumpy(size_sample))
-
-            lateral_ratio = cam_px_size / recon_vx_size  # px size in vx
-            axial_ratio = dy_step_size * np.tan(alpha_rad) / recon_vx_size  # distance between planes in vx
-            z_halfsize = 2 * axial_ratio
-            y_halfsize = 2 * lateral_ratio
-            x_halfsize = 2 * lateral_ratio
-            k_mesh = np.meshgrid(np.linspace(-z_halfsize, z_halfsize, int(np.ceil(2 * z_halfsize))),
-                                 np.linspace(-y_halfsize, y_halfsize, int(np.ceil(2 * y_halfsize))),
-                                 np.linspace(-x_halfsize, x_halfsize, int(np.ceil(2 * x_halfsize))), indexing='ij')
-
-            k_prime_z = k_mesh[0]  # *np.cos(alpha) - k_mesh[1]*np.sin(alpha)
-            k_prime_y = k_mesh[1]  # k_mesh[0]*np.sin(alpha) + k_mesh[1]*np.cos(alpha)
-            k_prime_x = k_mesh[2]
-            sigma_lat = lateral_ratio / 2.355
-            sigma_ax = axial_ratio / 2.355
-
-            kernel = np.exp(-(
-                        k_prime_x ** 2 / (2 * sigma_lat ** 2) + k_prime_y ** 2 / (2 * sigma_lat ** 2) + k_prime_z ** 2 / (
-                            2 * sigma_ax ** 2)))
-            # kernel = (np.ones_like(k_prime_x) - np.sqrt(
-            #     (k_prime_x / lateral_ratio) ** 2 + (k_prime_y / lateral_ratio) ** 2 + (k_prime_z / axial_ratio) ** 2)).clip(
-            #     0)
-            cupy_kernel = cp.array(kernel)
-            """Reconstruct"""
-            dataOnes = cp.ones_like(adjustedData)
-            threadsperblock = 8
-            blocks_per_grid_z = (size_data_host[0] + (threadsperblock - 1)) // threadsperblock
-            blocks_per_grid_y = (size_data_host[1] + (threadsperblock - 1)) // threadsperblock
-            blocks_per_grid_x = (size_data_host[2] + (threadsperblock - 1)) // threadsperblock
-            invNNTransform[(blocks_per_grid_z, blocks_per_grid_y, blocks_per_grid_x),
-                           (threadsperblock, threadsperblock, threadsperblock)](dataOnes, invTransfOnes, M, 0)
-
-            invNNTransform[(blocks_per_grid_z, blocks_per_grid_y, blocks_per_grid_x),
-                           (threadsperblock, threadsperblock, threadsperblock)](adjustedData, recon_canvas, M, 0)
-
-            interpolatedData = cpsig.fftconvolve(recon_canvas, cupy_kernel, mode='same')
-            interpolatedHtFromOnes = cpsig.fftconvolve(invTransfOnes, cupy_kernel, mode='same').clip(
-                0.1)  # Avoid divide by zero
-            reconstructed = cp.asnumpy(cp.divide(interpolatedData, interpolatedHtFromOnes))
+            dataOnes = cp.ones(data_correct_axes.shape)
+            invTransfOnes = cp.zeros(size_sample, dtype=dt)
+            gaussDistribTransform[(blocks_per_grid_z, blocks_per_grid_y, blocks_per_grid_x),
+                                  (threadsperblock, threadsperblock, threadsperblock)](dataOnes, invTransfOnes, M, sigma_z, sigma_y, sigma_x, z_halfsize, y_halfsize, x_halfsize)
+            del dataOnes
+            invTransfOnes = invTransfOnes.clip(0.01)
+            adjustedData = cp.array(data_correct_axes, dtype=dt)
+            adjustedData = cp.subtract(adjustedData, camera_offset).clip(0)
+            recon_canvas = cp.zeros(size_sample, dtype=dt)
+            gaussDistribTransform[(blocks_per_grid_z, blocks_per_grid_y, blocks_per_grid_x),
+                                  (threadsperblock, threadsperblock, threadsperblock)](adjustedData, recon_canvas, M, sigma_z, sigma_y, sigma_x, z_halfsize, y_halfsize, x_halfsize)
+            reconstructed = cp.asnumpy(cp.divide(recon_canvas, invTransfOnes))
+            del adjustedData
+            del recon_canvas
+            del invTransfOnes
+            mempool.free_all_blocks()
         except cp.cuda.memory.OutOfMemoryError:
-            self.__logger.warning('Out of memory, trying ndi affine transform')
-            mempool.free_all_blocks()
-            try:
-                affine_transform(adjustedData, cp.linalg.inv(M), offset=0.0, output_shape=recon_canvas.shape,
-                                       output=recon_canvas, order=3, mode='constant', cval=0.0, prefilter=True,
-                                       texture_memory=False)
-                self.__logger.warning('Performed ndi affine transform')
-                return cp.asnumpy(recon_canvas)
-            except:
-                return None
-        finally:
-            mempool.free_all_blocks()
+            print('Out of memory')
+            return None
 
         return reconstructed
 
